55b06a6e1fec5d6e7b44c7acf197a762cbfc6786
AmberFatima4 <emberfatima23@gmail.com>
2024-05-08 04:28:53 +0500

    updated

---
+++ chapter 1/app.js
@@ -0,0 +1,19 @@
+// task 1
+
+alert("Welcome To Our Website! We are glad that you visit here");
+
+// task 2
+
+alert("Error! Please enter a valid password");
+
+// task 3
+
+alert("Welcome to the JS land \n Happy coding ")
+
+// task 4
+
+alert("Welcome To JS Land");
+alert("Happy Coding")
+
+// task 5
+alert("Hello... I can run JS through ny web browser's console")
