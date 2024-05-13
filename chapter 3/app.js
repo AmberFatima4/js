0d3e36f84d6a70911d3e02c446a706773b4d7809
AmberFatima4 <emberfatima23@gmail.com>
2024-05-13 07:24:44 +0500

    chapters done

---
+++ chapter 3/app.js
@@ -0,0 +1,27 @@
+// TASK 1
+
+var age = 15;
+
+alert(`I am ${age} years old`);
+
+// TASK 2
+
+var visits = 14;
+
+alert(`You have visited this site ${visits} times`);
+
+// TASK 3
+
+var birthYear = 1999;
+
+document.write(`My birth year is ${birthYear} <br> Data type of my declared variable is number`);
+
+// TASK 4
+
+var visitorName = "John Doe";
+
+var productTitle = "T-shirt";
+
+var quantity = 5;
+
+document.write(`<b>${visitorName}</b> ordered <b>${quantity} ${productTitle}</b>(s) on XYZ Clothing store`);
